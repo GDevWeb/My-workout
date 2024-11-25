24ff160249ee2b9017d30d110a60392744d8a105
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-11-25 13:29:45 +0100

    signupform create + test; todo ajouter la validation

--- my-workout/src/pages/Signup.jsx
+++ my-workout/src/pages/Signup.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase/firebase";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  // 1.State:
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 2.Functions:
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/dashboard"); // Redirection après inscription
+    } catch (error) {
+      console.error("Erreur lors de l'inscription:", error.message);
+    }
+  };
+  return (
+    <form onSubmit={handleSignup}>
+      <h1>Inscription</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">S&apos;inscrire</button>
+    </form>
+  );
 };
 
 export default Signup;
