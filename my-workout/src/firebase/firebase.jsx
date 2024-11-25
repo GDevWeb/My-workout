52730d0a469c206078e45968aa753dce91c17f38
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-11-25 11:56:52 +0100

    init firebase + firebase.js

---
+++ my-workout/src/firebase/firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQgmISGsEYLrKKMWbLq-iVEuUEMYA6KA4",
+  authDomain: "my-workout-c79c2.firebaseapp.com",
+  projectId: "my-workout-c79c2",
+  storageBucket: "my-workout-c79c2.firebasestorage.app",
+  messagingSenderId: "1088084142844",
+  appId: "1:1088084142844:web:dd06d3566072aacef30579",
+  measurementId: "G-89C6GEZVDR",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
